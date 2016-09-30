8efa01924ed1901bb756ab0ad340bdfa69412712
jbecares <julibecares@hotmail.com.ar>
2016-09-29 22:24:28 -0300

    Update Test.php

--- tests/Test.php
+++ tests/Test.php
@@ -38,16 +38,26 @@ class TarjetaTest extends TestCase {
 	$tarje->recargar(272);
 	$tarje->pagar($bondi,"18.52","15/09/2016");
 	$this->assertEquals($tarje->saldo(), (320-8), "Cuando cargo 272 deberia tener finalmente 320 y paga 8 de pasaje");
-	
-
   }
 
   public function testTransbordoConMedio() {
-
+	$bondi1= new Colectivos("144");
+	$tarje= new Tarjetas("estudiante", "Medio boleto");
+	$tarje->recargar(272);
+	$tarje->pagar($bondi1,"18.52","15/09/2016");
+	$bondi= new Colectivos("145");
+	$tarje->pagar($bondi,"19.12","15/09/2016");
+	$this->assertEquals($tarje->saldo(), (320-1.32-4), "Cuando cargo 272 deberia tener finalmente 320 y paga 4 el primer viaje y 1.32 de trasbordo");	
   }
 	
   public function testTransbordoSinMedio() {
-
+	$tarje= new Tarjetas("movinormal", "Normal");
+	$tarje->recargar(272);
+	$bondi= new Colectivos("144");
+	$tarje->pagar($bondi,"18.52","15/09/2016");
+        $bondi1= new Colectivos("145");
+	$tarje->pagar($bondi1,"19.12","15/09/2016");
+	$this->assertEquals($tarje->saldo(), (320-8-2.64), "Cuando cargo 272 deberia tener finalmente 320 y paga 8 el primer viaje y 2.64 de trasbordo");
   }
 
   public function testNoTransbordoMedio() {
