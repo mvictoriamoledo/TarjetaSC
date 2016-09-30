590672c52286362a5518af4217916f4fb41f344a
mvictoriamoledo <mvmoledo@gmail.com>
2016-09-30 00:06:41 -0300

    Update Test.php

--- tests/Test.php
+++ tests/Test.php
@@ -28,7 +28,14 @@ class TarjetaTest extends TestCase {
   }
 
   public function testTransbordo() {
-
+	$bondi= new Colectivos("144");
+	$tarje= new Tarjetas("estudiante", "Medio boleto");
+	$tarje->recargar(272);
+	$tarje->pagar($bondi,"21.52","30/09/2016");
+	
+	$bondi2= new Colectivos("128");
+	$tarje->pagar($bondi,"22.05","30/09/2016");
+	$this->assertEquals($tarje->saldo(), (272-1.32-4), "cargo 271, pero se cargan 320. El primer viaje me sale 4 y el segundo 1,32");
   }
 
   public function testNoTransbordo() {
