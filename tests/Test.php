817928cc75de97c73bd69e77c643bb7aadb30310
mvictoriamoledo <mvmoledo@gmail.com>
2016-10-13 14:36:46 -0300

    Update Test.php

--- tests/Test.php
+++ tests/Test.php
@@ -23,8 +23,8 @@ class TarjetaTest extends TestCase {
 
   }
   public function testPagarBici() {
-	$bici= new Bicicleta("1234");
-	$tarje= new Tarjetas("ViajeNormal", "PaseEntero");
+	$bici= new Bicicletas("1234");
+	$tarje= new Tarjetas("normal", "movi estandar");
 	$tarje->recargar(272);
 	$tarje->pagar($bici,"18.52","15/09/2016");
 	$this->assertEquals($tarje->saldo(), (320-12), "Cuando cargo 272 deberia tener finalmente 320 y pagar 12 de pasaje");
