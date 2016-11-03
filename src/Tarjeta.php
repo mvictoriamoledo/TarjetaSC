336aa2824e28ca3c80e0bc10e1cb1ba3fd3c3cf5
mvictoriamoledo <mvmoledo@gmail.com>
2016-11-03 17:26:54 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,7 +3,7 @@
 namespace Poli\Tarjeta;
 
 interface Tarjeta{
-	public function pagar($transporte, $hora, $fecha);
+	public function pagar($transporte,$dia, $hora, $fecha);
 	public function recargar($monto);
 	public function saldo();
 	
