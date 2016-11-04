58ebee769418ea11e4501952c10aee8d1e72dfbe
jbecares <julibecares@hotmail.com.ar>
2016-11-04 16:17:19 -0300

    Update Tarjetas.php

--- src/Tarjetas.php
+++ src/Tarjetas.php
@@ -60,13 +60,13 @@ class Tarjetas implements Tarjeta{
 							$this->monto=2.64;
 						}
 						
-						else if(($hora>22||$hora<6) && ($hora-$this->boleto->darhora())<=1.3 && ($dia==$this->boleto->dardia()||($dia-$this->boleto->dardia())==1)){
+						else if{($hora>22||$hora<6) && ($hora-$this->boleto->darhora())<=1.3 && ($dia==$this->boleto->dardia()||($dia-$this->boleto->dardia())==1)){
 							#trasbordo
 							$this->saldo=$this->saldo-2.64;
 							$this->monto=2.64;
 						}
 						
-						else if($hora<22 && $hora>14 && ($hora-$this->boleto->darhora())<=1.3 && ($fecha=="sabado"||$fecha=="feriado") && $dia==$this->boleto->dardia()){
+						else if{$hora<22 && $hora>14 && ($hora-$this->boleto->darhora())<=1.3 && ($fecha=="sabado"||$fecha=="feriado") && $dia==$this->boleto->dardia()){
 							#trasbordo
 							$this->saldo=$this->saldo-2.64;
 							$this->monto=2.64;
