81401cb070b9a03e814e3c57b185b9f59d02cd8c
mvictoriamoledo <mvmoledo@gmail.com>
2016-11-08 11:01:24 -0300

    Update Tarjetas.php

--- src/Tarjetas.php
+++ src/Tarjetas.php
@@ -40,7 +40,7 @@ class Tarjetas implements Tarjeta{
 					if($hora>=6 && $hora<=22  && ($hora-$this->viaje->darhora())<= 1)
 					{
 						
-						$this->saldo=$this->saldo-(($this->monto*33)/100);
+						$this->saldo=$this->saldo-$this->monto;
 						$this->monto=($this->monto*33)/100;
 					}
 					
