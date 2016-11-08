6bc7f972cf66aa7b750ab22d09715ac1f7bf25eb
mvictoriamoledo <mvmoledo@gmail.com>
2016-11-08 10:38:09 -0300

    Update Tarjetas.php

--- src/Tarjetas.php
+++ src/Tarjetas.php
@@ -64,16 +64,16 @@ class Tarjetas implements Tarjeta{
 					
 						else 
 						{	
-						if( ($hora-$this->viaje->darhora())<= 1.30)
-						{
-							$this->monto=($this->monto*33)/100;
-							$this->saldo=$this->saldo-$this->monto;
-						}	
+							if( ($hora-$this->viaje->darhora())<= 1.30)
+							{
+								$this->monto=($this->monto*33)/100;
+								$this->saldo=$this->saldo-$this->monto;
+							}	
 						}
 					}
 				}
 				else 
-					if ($dia()=="Domingo" || $dia()=="Feriado" &&$this->tipo!="medio boleto")
+				{	if ($dia()=="Domingo" || $dia()=="Feriado" &&$this->tipo!="medio boleto")
 					{
 						if(($hora())>=6 && ($hora())<=22 && ($hora-$this->viaje->darhora())<= 1.30)
 						{
