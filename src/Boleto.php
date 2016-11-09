2486153e73d8d7d1fce59c65ea190703efa84c16
mvictoriamoledo <mvmoledo@gmail.com>
2016-11-09 15:29:49 -0300

    Update and rename Viajes.php to Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace Poli\Tarjeta;
-
-class Viajes{
-	public $viajes;
+class Boletos{
 	protected $fecha;
 	protected $hora;
 	protected $tipo;
@@ -12,17 +10,35 @@ class Viajes{
 	protected $idtarjeta;
 	protected $monto;
 	protected $dia;
-	protected $boleto;
-	public function __construct(){
-		$this->boleto= new Boleto;
+	public function pedirdatosultimoviaje($nrolinea,$monto,$fecha,$hora,$saldo,$nombre,$dia){
+		$this->monto=$monto;
+		
+		$this->nrotransporte=$nrolinea;
+		$this->fecha=$fecha;
+		$this->hora=$hora;
+	
+		$this->saldo=$saldo;
+		
+		$this->idtarjeta=$nombre;
+		
+		$this->dia=$dia;
+		
+		if($monto==8){
+			$this->tipo="Normal";	
+		}
+		if($monto==4){
+			$this->tipo="Medio";	
+		}
+		if($monto==1.32||$monto==2.64){
+			$this->tipo="Trasbordo";	
+		}
+		if($monto==-8){
+			$this->tipo="Viaje Plus";	
+		}
+		if($monto==0){
+			$this->tipo="Pase Libre";	
+		}
 	}
-	
-	
-	/*public function nuevoViaje( $boleto){
-		$this->viajes[i] = $boleto
-		$i++;
-	}*/
-	
 	
 	public function darnombre(){
 		return $this->idtarjeta;
@@ -48,7 +64,4 @@ class Viajes{
 	public function dardia(){
 		return $this->dia;
 	}
-		
-	
 }
-
